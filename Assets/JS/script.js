2c6e18a2d6f58cfcb20ebbfea780a051f765a63b
Maria Vallejo <maria.vallejo29@yahoo.com>
2021-08-24 03:32:15 -0500

    Rewrite classes and add coler code

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,13 +1,40 @@
 var timeDisplayEl = $('#currentDay');
 var timeBlockC = $('.container');
 
+
 // Shows current day
 var rightNow = moment().format('dddd, MMMM Do');
 timeDisplayEl.text(rightNow);
-// timeblock with standard bussiness hour
 
 //timeblock color coded whether past,present,or future
+function colorCode() {
+    var currentTime = parseInt(moment().format('H'));
+    var timeIds = new Array();
+    $(".time-block").each(function () {
+        timeIds.push(this.id);
+        
+    });
+    for (let i = 0; i < timeIds.length; i++) {
+        if (parseInt(timeIds[i]) < currentTime) {
+            $("#"+timeIds[i]).addClass('past');
+            console.log(timeIds[i]);
+            console.log(currentTime);
+        } else if (parseInt(timeIds[i]) === currentTime) {
+            $("#"+timeIds[i]).addClass('present');
+            console.log(timeIds[i]);
+            console.log(currentTime);
+        } else if (parseInt(timeIds[i]) > currentTime){
+            $("#"+timeIds[i]).addClass('future');
+            console.log(timeIds[i]);
+            console.log(currentTime);
+        }
+       
+    }
+    
+    
+}
 
+colorCode();
 //click event on timeblock-> enter an event
 
 //save btn on that click event then saved to local storage
