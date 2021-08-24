de95cd242b149368f8f993458a01b4410d5648bf
Maria Vallejo <maria.vallejo29@yahoo.com>
2021-08-24 00:47:30 -0500

    Add Timeblocks

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,15 @@
 var timeDisplayEl = $('#currentDay');
+var timeBlockC = $('.container');
 
-
+// Shows current day
 var rightNow = moment().format('dddd, MMMM Do');
 timeDisplayEl.text(rightNow);
+// timeblock with standard bussiness hour
 
+//timeblock color coded whether past,present,or future
+
+//click event on timeblock-> enter an event
+
+//save btn on that click event then saved to local storage
+
+//saved events dont go away when refreshed
