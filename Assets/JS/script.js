acb5d13f533b438fad470fa10a19b8ce0a82e307
Maria Vallejo <maria.vallejo29@yahoo.com>
2021-08-25 18:24:51 -0500

    add event saved to local storage

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,5 +1,6 @@
 var timeDisplayEl = $('#currentDay');
 var timeBlockC = $('.container');
+var saveBtn = $('.saveBtn');
 
 
 // Shows current day
@@ -12,31 +13,49 @@ function colorCode() {
     var timeIds = new Array();
     $(".time-block").each(function () {
         timeIds.push(this.id);
-        
+
     });
     for (let i = 0; i < timeIds.length; i++) {
         if (parseInt(timeIds[i]) < currentTime) {
-            $("#"+timeIds[i]).addClass('past');
-            console.log(timeIds[i]);
-            console.log(currentTime);
+            $("#" + timeIds[i]).addClass('past');
         } else if (parseInt(timeIds[i]) === currentTime) {
-            $("#"+timeIds[i]).addClass('present');
-            console.log(timeIds[i]);
-            console.log(currentTime);
-        } else if (parseInt(timeIds[i]) > currentTime){
-            $("#"+timeIds[i]).addClass('future');
-            console.log(timeIds[i]);
-            console.log(currentTime);
+            $("#" + timeIds[i]).addClass('present');
+        } else if (parseInt(timeIds[i]) > currentTime) {
+            $("#" + timeIds[i]).addClass('future');
         }
-       
+
     }
-    
-    
+
 }
 
 colorCode();
 //click event on timeblock-> enter an event
-
 //save btn on that click event then saved to local storage
+var txt = new Array();
+var textIds = new Array();
+var text= [];
+saveBtn.on('click', function () {
+    $(".description").each(function () {
+        textIds.push(this.id);
 
+    });
+    for (let i = 0; i < textIds.length; i++) {
+        var decription = $('#' + textIds[i]).val();
+        text.push(decription);
+    }
+    var eventTxt = {
+        textIds,
+        text,
+    }
+
+    window.localStorage.setItem('event', JSON.stringify(eventTxt));
+});
 //saved events dont go away when refreshed
+var retrieveEvent = localStorage.getItem('event');
+var objectEvent = JSON.parse(retrieveEvent);
+
+
+console.log(objectEvent.textIds[0]);
+console.log(objectEvent.text[0]);
+
+
